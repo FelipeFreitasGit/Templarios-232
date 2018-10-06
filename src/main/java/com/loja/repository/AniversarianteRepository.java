a701403bb5985e6f58b34a23d76ad9222401032e
FelipeFreitasGit <vasconcellosdefreitas@outlook.com>
2018-10-06 17:12:45 -0300

    processo automatico email aniversarinate precisa ser completado

--- src/main/java/com/loja/repository/AniversarianteRepository.java
+++ src/main/java/com/loja/repository/AniversarianteRepository.java
@@ -1,9 +1,16 @@
 package com.loja.repository;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.loja.models.Aniversariante;
 
 public interface AniversarianteRepository extends JpaRepository<Aniversariante, Long>{
+	
+	@Query("SELECT a FROM Aniversariante a WHERE month(a.dataAniversario) = month(now())")
+	List<Aniversariante> findAniversarianteByDataAniversario(Date data);
 	
 }
--- src/main/java/com/loja/tarefasAutomaticas/DisparaAniversariante.java
+++ src/main/java/com/loja/tarefasAutomaticas/DisparaAniversariante.java
@@ -1,8 +1,15 @@
 package com.loja.tarefasAutomaticas;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import com.loja.models.Aniversariante;
+import com.loja.repository.AniversarianteRepository;
+import com.loja.services.EmailService;
 
 @Component
 @EnableScheduling
@@ -10,8 +17,37 @@ public class DisparaAniversariante {
 	
 	private static final String TIME_ZONE = "America/Sao_Paulo";
 	
-	@Scheduled(cron = "0 0 12 * * *", zone = TIME_ZONE)
+	@Autowired
+	private AniversarianteRepository aniversarianteRepository;
+	
+	@Autowired
+	private EmailService email;
+	
+	@Scheduled(cron = "0/20 * * * * ?", zone = TIME_ZONE)
 	public void mandaEmailDeAniversário() {
-		System.out.println("disparando teste");
-	}
+		
+		List<Aniversariante> aniversariantes = aniversarianteRepository.findAll();
+						
+		for (Aniversariante aniversariante : aniversariantes) {
+			
+			List<Aniversariante> getDatasAniversariantes = 
+			  aniversarianteRepository.findAniversarianteByDataAniversario(aniversariante.getDataAniversario());
+			
+			if (getDatasAniversariantes != null) {
+				
+				System.out.println("Preparando email...");
+				
+				
+					
+					email.enviar(
+							aniversariante.getEmail(), 
+							"Aniversáriantes - Loja Templários!", 
+							"Temos aniversáriantes hoje, de parabéns para " + "");
+				
+				
+				System.out.println("email enviado!");
+			}
+		}
+		
+	} 
 }
